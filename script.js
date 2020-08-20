64ec6246ba7423f3453a71877f212e7d7feb933c
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-20 17:34:09 -0400

    fixed buttons on portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+<button class="nav" class="float-left submit-button" >Home</button>
+
+
+    document.getElementById("myButton").onclick = function () {
+        location.href = "www.yoursite.com";
+    };
