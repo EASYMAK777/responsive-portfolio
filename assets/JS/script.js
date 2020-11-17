359eb2f07d5ea58f48cb64a42f91507ce589ee72
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-17 14:51:43 -0500

    initializing firebase js

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBRoem3upMZ9aQTA_KR9tQP3gzRWzDw1M0",
+    authDomain: "portfolio-html-fc187.firebaseapp.com",
+    databaseURL: "https://portfolio-html-fc187.firebaseio.com",
+    projectId: "portfolio-html-fc187",
+    storageBucket: "portfolio-html-fc187.appspot.com",
+    messagingSenderId: "1021172127758",
+    appId: "1:1021172127758:web:36e149b4d11c96f51d045f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
